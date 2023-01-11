86ab6204d21803ae06864e8c0b9aea82902be9a4
praveenkg <kgpraveen2904@gmail.com>
2023-01-11 18:59:31 +0530

    Solved helloWorld and repeatString exercises.
    It was complex to understand at first, but got easy once I actually did it lol.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, times) {
+    let result = "";
+    if (times < 0) {
+        return ("ERROR")
+    }
+    for (let count = 0; count < times; count++) {
+        result = result + str;
+    }
+    return (result);
 };
 
 // Do not edit below this line
