87bee37d580f582e715f244e221e4447467cd24e
praveenkg <kgpraveen2904@gmail.com>
2023-01-14 12:35:45 +0530

    Completely finished all stringReverse & removeFromArray Exercises. Everything is done.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var strReverse = "";
+    for (let i = 0 ; i < str.length ; i++) {
+        strReverse = str[i] + strReverse;
+    }
+    return strReverse;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrays, removeMe0, removeMe1, removeMe2, removeMe3) {
+    var indices = new Array;
+    var removeArrays = new Array;
 
+    removeArrays.push(removeMe0);
+    if (removeMe1 != null) {
+        removeArrays.push(removeMe1);
+    }
+    if (removeMe2 != null) {
+        removeArrays.push(removeMe2);
+    }
+    if (removeMe3 != null) {
+        removeArrays.push(removeMe3);
+    }
+
+    for (let i = 0; i < removeArrays.length; i++) {
+        if (arrays.includes(removeArrays[i])) {
+            indices.push(arrays.indexOf(removeArrays[i]));
+        }
+    }
+
+    indices.sort(function (a, b) { return a - b });
+
+    for (i = indices.length - 1; i >= 0; i--) {
+        arrays.splice(indices[i],1);
+    }
+
+    return arrays;
 };
 
 // Do not edit below this line
