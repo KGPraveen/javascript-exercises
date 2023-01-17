07ad258c6fb8180685266076463666d716653ba0
praveenkg <kgpraveen2904@gmail.com>
2023-01-17 09:34:45 +0530

    Finished sumAll, it was easier than removeFromArray lol

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+
+    if (!(typeof (a) == "number" && typeof (b) == "number")) {
+        return ("ERROR")
+    }
+
+    a = Number(a);
+    b = Number(b);
+
+    if (a < 0 || b < 0) {
+        return ("ERROR");
+    }
+
+    let limit, begin;
+    let sum = 0;
+
+    if (a < b) {
+        limit = b;
+        begin = a;
+    } else {
+        limit = a;
+        begin = b;
+    }
+
+    for (let i = begin; i <= limit; i++) {
+        sum = sum + i;
+    }
+
+    return (sum);
 
 };
 
